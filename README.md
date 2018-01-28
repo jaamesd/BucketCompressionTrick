4bae25ef72a1ebf30056d7a57df9f9f50e2c9092
James Deng <james.deng@alumni.ubc.ca>
2018-01-27 16:04:52 -0800

    pretty

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CompressionTrick
-Cool trick to store 4 5-bit values in one 16 value.
+
+Cool trick to store 4 5-bit values in one 16 bit value.
+
+This works because there are 3876 possible unique values for a set of 4 4 bit values whereas there are 65536 possible unique values for a 16 bit integer. log2(3876) = 11.92 so if we can map all possible values to a 12 bit representation which leaves us with 4 bits to store the 5th bit for the extra numbers. This makes sense because 65536 / 3876 = 16.9, and log2(16.9) = 4.08 and 4.08 + 11.92 = 16.
+
+Another way to think about it is
+
--- main.cpp
+++ main.cpp
@@ -8,19 +8,11 @@
 
 using namespace std;
 
-template <class T, std::size_t N>
-ostream& operator<<(ostream& oss, const array<T, N>& arrays)
-{
-    copy(arrays.cbegin(), arrays.cend(), ostream_iterator<T>(oss, " "));
-    return oss;
-}
-
-
 class BucketCompressor
 {
 private:
     static bool mInitialized;
-    static uint16_t mMaxCodeValue;
+    static uint16_t mMaxIndex;
     static array<uint16_t, UINT16_MAX> mFwdLut;
     static array<uint16_t, UINT16_MAX / 4> mRevLut;
 
@@ -39,21 +31,23 @@ private:
                 {
                     for (uint8_t l = 0; l <= k; l++)
                     {
-                        uint16_t sortedWord = i << 0 * 4 |
-                                              j << 1 * 4 |
-                                              k << 2 * 4 |
-                                              l << 3 * 4;
-                        /*  0 |<--------------------->| 32
-                         *    |  i  |  j  |  k  |  l  |
-                         *  The largest nibble is stored on the least
-                         *  significant bits so we
+                        uint16_t sortedValues = i << 0 * 4 |
+                                                j << 1 * 4 |
+                                                k << 2 * 4 |
+                                                l << 3 * 4;
+                        /*  0                       16
+                         *  |<-----  16 bits  ----->|
+                         *  |  i  |  j  |  k  |  l  |
+                         *  where i >= j >= k >= l, all are 4 bit values
+                         *  the largest value is stored in the least significant
+                         *  bits so it's monontically increasing just like index
                          */
-                        mFwdLut[sortedWord] = mMaxCodeValue;
-                        mRevLut[mMaxCodeValue] = sortedWord;
+                        mFwdLut[sortedValues] = mMaxIndex;
+                        mRevLut[mMaxIndex] = sortedValues;
                         cout << "mapping 0x" << hex << setfill('0') << setw(4)
-                            << mMaxCodeValue << " <----> " << "0x" << hex <<
-                            setfill('0') << setw(4) << sortedWord << endl;
-                        mMaxCodeValue++;
+                            << mMaxIndex << " <----> " << "0x" << hex <<
+                            setfill('0') << setw(4) << sortedValues << endl;
+                        mMaxIndex++;
                     }
                 }
             }
@@ -67,61 +61,102 @@ private:
     }
 
 public:
-    static void Compress(uint16_t &code, array<uint16_t, 4>dataIn)
+    static void Compress(uint16_t &code, array<uint8_t, 4>dataIn)
     {
         for (const auto &data : dataIn)
         {
             assert(data < 1 << 5);
         }
 
-        sort(dataIn.begin(), dataIn.end());
+        // Sort to decending order
+        sort(dataIn.begin(), dataIn.end(), greater<uint8_t>());
 
-        uint16_t sortedWord = (dataIn[0] >> 1) << 3 * 4 |
-                              (dataIn[1] >> 1) << 2 * 4 |
-                              (dataIn[2] >> 1) << 1 * 4 |
-                              (dataIn[3] >> 1) << 0 * 4;
+        /*  0                                5
+         *  |<----------  5 bits  ---------->|
+         *  |   value & 1   |   value >> 1   |
+         *  |             value              |
+         *  split value into most sigificant bits and least sigificant bit
+         */
 
-        code = Instance().mFwdLut[sortedWord] << 4 |
-                (dataIn[0] & 1) << 3 |
-                (dataIn[1] & 1) << 2 |
-                (dataIn[2] & 1) << 1 |
-                (dataIn[3] & 1) << 0;
+        /*  0                                   16
+         *  |<-----------  16 bits  ----------->|
+         *  | i >> 1 | j >> 1 | k >> 1 | l >> 1 |
+         *  |           sorted values           |
+         *  where i >= j >= k >= l, all are 5 bit values
+         *  use the most sigificant bits for each value to form the sorted word
+         *  the largest value is again stored in the least sigificant bits to
+         *  match our lookup tables
+         */
 
-        cout << "Compressed:\t" << dataIn << endl;
-        cout << "Into: " << code << endl;
+        uint16_t sortedValues = (dataIn[0] >> 1) << 0 * 4 |   // i
+                                (dataIn[1] >> 1) << 1 * 4 |   // j
+                                (dataIn[2] >> 1) << 2 * 4 |   // k
+                                (dataIn[3] >> 1) << 3 * 4;    // l
+
+        /*  0                               4               16
+         *  |<---------  4  bits  --------->|<-  12 bits  ->|
+         *  | i & 1 | j & 1 | k & 1 | l & 1 |   lut index   |
+         *  |                     code                      |
+         *  where i >= j >= k >= l
+         *  look up the index of sorted value in the lookup table we created and
+         *  store in the least significant 4 bits of the code, the least
+         *  sigificant bits of the values in sorted order so we can retrieve it
+         */
+        code = Instance().mFwdLut[sortedValues] << 4 |
+                (dataIn[0] & 1) << 0 |
+                (dataIn[1] & 1) << 1 |
+                (dataIn[2] & 1) << 2 |
+                (dataIn[3] & 1) << 3;
+
+        cout << "Compressed:\t{ ";
+
+        for (auto &data : dataIn)
+        {
+            cout << +data << " ";
+        }
+
+        cout << "} Into: 0x" << hex << setfill('0') << setw(4) << code << endl;
     }
 
-    static void Decompress(const uint16_t &code, array<uint16_t, 4>&dataOut)
+    static void Decompress(const uint16_t &code, array<uint8_t, 4>&dataOut)
     {
-        assert(code >> 4 < mMaxCodeValue);
+        assert(code >> 4 < mMaxIndex);
 
-        uint16_t sortedWord = Instance().mRevLut[code >> 4];
+        // use the lut index to retrieve the sorted values
+        uint16_t sortedValues = Instance().mRevLut[code >> 4];
 
-        dataOut[0] = (sortedWord >> (3 * 4) & 0xf) << 1 | (code & (1 << 3)) >> 3 ;
-        dataOut[1] = (sortedWord >> (2 * 4) & 0xf) << 1 | (code & (1 << 2)) >> 2 ;
-        dataOut[2] = (sortedWord >> (1 * 4) & 0xf) << 1 | (code & (1 << 1)) >> 1 ;
-        dataOut[3] = (sortedWord >> (0 * 4) & 0xf) << 1 | (code & (1 << 0)) >> 0 ;
+        // piece them back together
+        dataOut[0] = (sortedValues >> (0 * 4) & 0xf) << 1 | (code & (1 << 0)) >> 0 ;
+        dataOut[1] = (sortedValues >> (1 * 4) & 0xf) << 1 | (code & (1 << 1)) >> 1 ;
+        dataOut[2] = (sortedValues >> (2 * 4) & 0xf) << 1 | (code & (1 << 2)) >> 2 ;
+        dataOut[3] = (sortedValues >> (3 * 4) & 0xf) << 1 | (code & (1 << 3)) >> 3 ;
 
-        cout << "Decompressed:\t" << dataOut << endl;
-        cout << "From: " << code << endl;
+        cout << "Decompressed:\t{ ";
+
+        for (auto &data : dataOut)
+        {
+            cout << +data << " ";
+        }
+
+        cout << "} From: 0x" << hex << setfill('0') << setw(4) << code << endl;
     }
 
     static void Compress(uint16_t &code, uint32_t dataIn)
     {
         assert(dataIn <= ((1 << 20) - 1));
-        std::array<uint16_t, 4> buffer =
+        std::array<uint8_t, 4> buffer =
         {
-            static_cast<uint16_t>(dataIn >> (0 * 4) & 0xf),
-            static_cast<uint16_t>(dataIn >> (1 * 4) & 0xf),
-            static_cast<uint16_t>(dataIn >> (2 * 4) & 0xf),
-            static_cast<uint16_t>(dataIn >> (3 * 4) & 0xf),
+            static_cast<uint8_t>(dataIn >> (0 * 4) & 0xf),
+            static_cast<uint8_t>(dataIn >> (1 * 4) & 0xf),
+            static_cast<uint8_t>(dataIn >> (2 * 4) & 0xf),
+            static_cast<uint8_t>(dataIn >> (3 * 4) & 0xf),
         };
         Compress(code, buffer);
     }
 
     static void Decompress(const uint16_t &code, uint32_t &dataOut)
     {
-        std::array<uint16_t, 4> buffer;
+        std::array<uint8_t, 4> buffer;
         Decompress(code, buffer);
 
         dataOut = (buffer[0] & 0xf) << (0 * 4) |
@@ -132,36 +167,47 @@ public:
 };
 
 bool BucketCompressor::mInitialized;
-uint16_t BucketCompressor::mMaxCodeValue;
+uint16_t BucketCompressor::mMaxIndex;
 array<uint16_t, UINT16_MAX> BucketCompressor::mFwdLut;
 array<uint16_t, UINT16_MAX / 4> BucketCompressor::mRevLut;
 
 int main(int argc, char const *argv[])
 {
     uint16_t code;
-    srand(time(nullptr));
-    std::array<uint16_t, 4> dataIn;
-    std::array<uint16_t, 4> dataOut;
+    std::array<uint8_t, 4> dataIn;
+    std::array<uint8_t, 4> dataOut;
 
-    for (auto &data : dataIn)
+    for (uint8_t i = 0; i < 1 << 4; i++)
     {
-        data = static_cast<uint16_t>(rand() % (1 << 5));
+        for (uint8_t j = 0; j <= i; j++)
+        {
+            for (uint8_t k = 0; k <= j; k++)
+            {
+                for (uint8_t l = 0; l <= k; l++)
+                {
+                    cout << "----------------------------------------" << endl;
+                    dataIn[0] = i;
+                    dataIn[1] = j;
+                    dataIn[2] = k;
+                    dataIn[3] = l;
+                    BucketCompressor::Compress(code, dataIn);
+                    BucketCompressor::Decompress(code, dataOut);
+                    assert(dataIn == dataOut);
+                }
+            }
+        }
     }
 
-    BucketCompressor::Compress(code, dataIn);
-    BucketCompressor::Decompress(code, dataOut);
-    sort(dataIn.begin(), dataIn.end());
-    assert(dataIn == dataOut);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
+    srand(time(nullptr));
     while(1) {
-        cout << "----------------------------" << endl;
+        cout << "----------------------------------------" << endl;
         uint32_t x = rand() % (1 << 20);
         BucketCompressor::Compress(code, x);
         BucketCompressor::Decompress(code, x);
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
-
-
 
     return 0;
 }
